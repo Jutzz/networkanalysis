6e0bc3173773db85952f23cfe757e8e4ccbddef3
Jutzz <Jutzz149@gmail.com>
2026-06-21 22:21:32 +0200

    i3 basics

--- code/Indikator_03.R
+++ code/Indikator_03.R
@@ -1,8 +1,7 @@
 options(java.parameters = "-Xmx20G")
 library(r5r)
-library(sf)
-library(dplyr)
 library(tidyverse)
+library(sf)
 library(gpkg)
 library(raster)
 library(tidytransit)
@@ -33,21 +32,21 @@ Erreichbarkeit <- function(origins, destinations) {
   
   
   shortest_travel_times <- ttm %>%
-    group_by(from_id) %>%
+    group_by(to_id) %>%
     slice_min(travel_time_p01, with_ties = FALSE) %>%
     ungroup()
   
   grid_with_times <- polygrid100 %>%
-    filter(if ("Bevölkerung" %in% names(.)) Bevölkerung != 0 else TRUE) %>%
+    filter(if ("Einwohner" %in% names(.)) Einwohner != 0 else TRUE) %>%
     left_join(shortest_travel_times %>%
-                dplyr::select(from_id, to_id, travel_time_p01), by = c("id" = "from_id")) %>%
+                dplyr::select(from_id, to_id, travel_time_p01), by = c("id" = "to_id")) %>%
     mutate(start_time = as.POSIXct("2025-02-19 08:00:00")) %>%
-    mutate(end_time = start_time + minutes(travel_time_p01)) %>%
-    left_join(zentraleOrteid, by = c("to_id" = "id")) %>%
-    dplyr::select(!c("ART", "STAND", "KN7stellig")) %>%
-    rename("GN_ziel" = GN, "KN_ziel" = KN)
+    mutate(end_time = start_time + minutes(travel_time_p01)) #%>%
+    #left_join(zentraleOrteid, by = c("to_id" = "id")) %>%
+    #dplyr::select(!c("ART", "STAND", "KN7stellig")) %>%
+    #rename("GN_ziel" = GN, "core_ziel" = core_id)
   
-  st_write(grid_with_times, here("output/indikator_03.gpkg"), layer = paste0(timestamp, "_nrw_gtfs_polygrid_100m_", ort$GN, "_", poi_type, "_", modes_filename, "_", max_trip_duration,"min"), append = FALSE)
+  st_write(grid_with_times, here("output/indikator_03.gpkg"), layer = paste0("de_gtfs_polygrid_100m_shortest_", poi_type, "_", modes_filename, "_", max_trip_duration,"min"), append = FALSE)
 }
 
 #Wie oben, ohne filtern nach kürzester
@@ -70,14 +69,14 @@ Erreichbarkeit_nofiltering <- function(origins, destinations) {
   )
   
   grid_with_times <- polygrid100 %>%
-    filter(if ("Bevölkerung" %in% names(.)) Bevölkerung != 0 else TRUE) %>%
+    filter(if ("Einwohner" %in% names(.)) Einwohner != 0 else TRUE) %>%
     left_join(ttm %>%
                 dplyr::select(from_id, to_id, travel_time_p01), by = c("id" = "to_id")) %>%
     mutate(start_time = as.POSIXct("2025-02-19 08:00:00")) %>%
     mutate(end_time = start_time + minutes(travel_time_p01)) %>%
     left_join(zentraleOrteid, by = c("from_id" = "id")) %>%
     #dplyr::select(!c("ART", "STAND", "KN7stellig")) %>%
-    rename("GN_ziel" = GN, "KN_ziel" = parent_KN)
+    rename("GN_ziel" = GN, "core_ziel" = core_id)
   
   st_write(grid_with_times, here("output/indikator_03.gpkg"), layer = paste0("nrw_gtfs_polygrid_100m_all_destinations_subset", "_", poi_type, "_", modes_filename, "_", max_trip_duration,"min"), append = FALSE)
   return(ttm)
@@ -97,7 +96,7 @@ catchment_fun <- function(grid, overlay){
     group_by(GN, KN) %>%  
     summarize(
       count = n(),  # Count of intersecting features
-      bevölkerung_sum = sum(Bevölkerung, na.rm = TRUE
+      bevölkerung_sum = sum(Einwohner, na.rm = TRUE
       )
     )
   
@@ -108,7 +107,7 @@ catchment_fun <- function(grid, overlay){
     group_by(GN, KN) %>%  
     summarize(
       count = n(),  # Count of intersecting features
-      bevölkerung_catched_sum = sum(Bevölkerung, na.rm = TRUE)
+      bevölkerung_catched_sum = sum(Einwohner, na.rm = TRUE)
     )
   print("Catched Population Aggregation done")
   
@@ -117,7 +116,7 @@ catchment_fun <- function(grid, overlay){
     group_by(GN, KN) %>%  
     summarize(
       count = n(),  # Count of intersecting features
-      bevölkerung_uncatched_sum = sum(Bevölkerung, na.rm = TRUE)
+      bevölkerung_uncatched_sum = sum(Einwohner, na.rm = TRUE)
     )
   
   print("Uncatched Population Aggregation done")
@@ -148,7 +147,7 @@ gemeinden <- st_read(dsn = here("geodata/dvg1nw.gpkg"), layer = "gemeinden_regbe
 zentraleOrte <- st_transform(st_read(here("geodata/poi.gpkg"), "zo_pd3_cut0.17"), crs = crs(zensus_grid))
 zentraleOrteid <- st_drop_geometry(zentraleOrte) %>%
   mutate(id = core_id) %>%
-  select(id, GN)
+  dplyr::select(id, core_id, GN)
 
 #R5 Setup
 r5_network <- build_network(here("r5core_current"), verbose = FALSE, overwrite = FALSE)
@@ -161,17 +160,32 @@ polygrid100 <- zensus_grid %>%
 
 zensus_grid_df <- pointgrid_fun(polygrid100, "id")
 
-pois_df <- pois_fun(pois = get(poi_type))
+pois_df <- pois_fun(pois = get(poi_type), id_col = "core_id")
 
 #Erreichbarkeitsanalyse
 ttm <- Erreichbarkeit_nofiltering(origins = pois_df, destinations = zensus_grid_df)
 
 all_clear()
 
-zensusstats <- i3full %>%
+
+i3full <- st_read(here("output/indikator_03.gpkg"), "nrw_gtfs_polygrid_100m_all_destinations_subset_zentraleOrte_WALKTRANSIT_60min")
+
+
+cent <- st_centroid(i3full) %>%
+  st_join(st_transform(gemeinden, crs = st_crs(i3full)), st_intersects)
+
+cent_s <- st_drop_geometry(cent) %>%
+  dplyr::select(id, core_ziel, GN, KN) %>%
+  rename("origin_KN" = KN,
+         "origin_GN" = GN)
+
+i3full_coded <- i3full %>%
+  left_join(cent_s)
+
+zensusstats <- i3full_coded %>%
   st_drop_geometry() %>%
-  group_by(origin_KN) %>%
-  mutate(pop_sum = sum(Bevölkerung)) %>%
+  group_by(origin_KN,GN_ziel) %>%
+  mutate(pop_sum = sum(Einwohner)) %>%
   dplyr::select(origin_GN, origin_KN, pop_sum) %>%
   distinct()
 
@@ -179,7 +193,7 @@ i3fullstats <- i3full %>%
   st_drop_geometry() %>%
   filter(ziel_is_origin == FALSE) %>%
 group_by(origin_KN) %>%
-  mutate("sum_orignotdest" = sum(Bevölkerung)) %>%
+  mutate("sum_orignotdest" = sum(Einwohner)) %>%
   dplyr::select(9:12) %>%
   distinct() %>%
   left_join(zensusstats, by = c("origin_GN", "origin_KN")) %>%
@@ -200,7 +214,7 @@ i3all_noaccess <- i3all %>%
 i3all_noaccess_stats <- i3all_noaccess %>%
   st_drop_geometry() %>%
   group_by(origin_KN) %>%
-  mutate(uncatched = sum(Bevölkerung)) %>%
+  mutate(uncatched = sum(Einwohner)) %>%
   dplyr::select(origin_GN, origin_KN, uncatched) %>%
   distinct()
 
@@ -213,3 +227,6 @@ zensus_populated <- st_read("grids.gpkg", "zensusgrid_populated")
 i3_cgn <- i3all %>%
   filter(GN_ziel == "Köln" | is.na(travel_time_p01))
 
+catchment_fun(grid = i3full_coded, overlay = st_transform(gemeinden, crs = st_crs(i3full_coded)))
+
+              
